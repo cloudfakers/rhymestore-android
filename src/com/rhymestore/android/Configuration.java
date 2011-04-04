d7b7cfca57ab66be3c5f710e4c1b2d4b34859c1d
Vivien Mahe <vivouneuh@gmail.com>
2011-04-05 00:18:09 +0200

    Deleted useless files

--- src/com/rhymestore/android/Configuration.java
+++
@@ -1,9 +0,0 @@
-package com.rhymestore.android;
-
-public class Configuration
-{
-    public static final String PREFS_NAME = "RhymestorePrefs";
-
-    public static final String PREFS_PREFIX_KEY = "rhymestore_";
-
-}
